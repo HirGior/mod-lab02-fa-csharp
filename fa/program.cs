830c3a44441efd8f0e02c67be851d7c94237d9a6
HirGior <dimasamots@yandex.ru>
2026-04-05 20:46:53 +0300

    LAB2 compl

--- fa/program.cs
+++ fa/program.cs
@@ -16,25 +16,100 @@ namespace fans
 
   public class FA1
   {
+    private State start = new State { Name = "staet" };
+    private State one1 = new State { Name = "one1" };
+    private State one0 = new State { Name = "one0" };
+    private State accept = new State { Name = "accept", IsAcceptState = true };
+    private State dead = new State { Name = "dead" };
+
+    public FA1()
+    {
+      start.Transitions['0'] = one0;
+      start.Transitions['1'] = one1;
+
+      one1.Transitions['1'] = one1;
+      one1.Transitions['0'] = accept;
+
+      one0.Transitions['0'] = dead;
+      one0.Transitions['1'] = accept;
+
+      accept.Transitions['0'] = dead;
+      accept.Transitions['1'] = accept;
+
+      dead.Transitions['0'] = dead;
+      dead.Transitions['1'] = dead;
+    }
+    
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = start;
+        foreach (var c in s)
+        {
+            current = current.Transitions[c];
+        }
+        return current.IsAcceptState;
     }
   }
 
   public class FA2
   {
+    private State ee = new State { Name = "ee" };
+    private State oe = new State { Name = "oe" };
+    private State eo = new State { Name = "eo" };
+    private State oo = new State { Name = "oo", IsAcceptState = true };
+
+    public FA2()
+    {
+      ee.Transitions['0'] = oe;
+      ee.Transitions['1'] = eo;
+
+      oe.Transitions['0'] = ee;
+      oe.Transitions['1'] = oo;
+
+      eo.Transitions['0'] = oo;
+      eo.Transitions['1'] = ee;
+
+      oo.Transitions['0'] = eo;
+      oo.Transitions['1'] = oe;
+    }
+    
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = ee;
+        foreach (var c in s)
+        {
+            current = current.Transitions[c];
+        }
+        return current.IsAcceptState;
     }
   }
   
   public class FA3
   {
+    private State start = new State { Name = "start" };
+    private State one = new State { Name = "one" };
+    private State accept = new State { Name = "accept", IsAcceptState = true };
+
+    public FA3()
+    {
+      start.Transitions['0'] = start;
+      start.Transitions['1'] = one;
+
+      one.Transitions['0'] = start;
+      one.Transitions['1'] = accept;
+
+      accept.Transitions['0'] = accept;
+      accept.Transitions['1'] = accept;
+    }
+    
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = start;
+        foreach (var c in s)
+        {
+            current = current.Transitions[c];
+        }
+        return current.IsAcceptState;
     }
   }
 
